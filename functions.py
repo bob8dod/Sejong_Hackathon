ea33c21ced1263d7fac7802b64b2db328f2c12cd
Seung-Il Park <72644713+bob8dod@users.noreply.github.com>
2021-10-23 23:34:18 +0900

    Update functions.py

--- functions.py
+++ functions.py
@@ -57,23 +57,6 @@ def return_keyword(review_data):
     nouns = return_nouns(review_data)  # 명사 추출
     vocab_sorted = count_noun(nouns)  # 명사 키워드
     check_vocab = [word for word in vocab_sorted if len(word[0])>1]  # 20개 출력
-    # JKS, JX_중요조사들
-    # josa = ['JKS', 'JX']  # 품사 중 조사에 대한 표현 저장 #+JKO, JKB, JKG, JKV, JKC, JC
-    # word_next_josa = {w: 0 for w, k in vocab_sorted}  # 단어 뒤에 조사가 붙는지에 대한 count를 저장하기 위한 딕셔너리
-    # for i in range(len(review_data_list)):
-    #     for word, value in vocab_sorted:  # 저장된 단어들 호출
-    #         for idx in range(len(review_data_list[i])):
-    #             if word == review_data_list[i][idx][0]:
-    #                 if idx + 1 < len(review_data_list[i]) and review_data_list[i][idx + 1][
-    #                     1] in josa:  # 해당 단어의 다음에 조사가 나온다면
-    #                     word_next_josa[word] += 1  # count 해줌
-    #
-    # word_josa_count = sorted(word_next_josa.items(), key=lambda x: x[1], reverse=True)[:20]  # count를 기준으로 sort 20개
-    # keyword_before = []
-    # for i in range(len(word_josa_count)):
-    #     for j in range(len(check_vocab)):
-    #         if word_josa_count[i][0] == check_vocab[j][0] and word_josa_count[i][1] > 1:  # 그냥 가장 많이 나온 단어들과 다음 단어가 조사가 나오는 단어들 중 을 선택
-    #             keyword_before.append(word_josa_count[i][0])
 
     return check_vocab[:10]
 
